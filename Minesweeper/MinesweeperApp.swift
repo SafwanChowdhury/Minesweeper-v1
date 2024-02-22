7b6ebe8e6d1d5e101743fea17022be5b421d6fea
Safwan Chowdhury <safwanrija@yahoo.co.uk>
2024-02-22 23:53:21 +0000

    Initial Commit

---
+++ Minesweeper/MinesweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinesweeperApp.swift
+//  Minesweeper
+//
+//  Created by Safwan Chowdhury on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MinesweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
